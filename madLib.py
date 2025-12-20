7c0259d03e7f0e6e4823ebab72fede5161bccf80
vidhi udasi <udasividhi2@gmail.com>
2025-12-20 21:06:01 +0530

    Create madLib.py

---
+++ madLib.py
@@ -0,0 +1,8 @@
+# Mad Libs is a word game where players fill in blanks in a story template with different parts of speech (nouns, verbs, adjectives, etc.) without knowing the story context
+
+ad=input("adjective: ")
+verb=input(" verb: ")
+verb2=input(" verb2: ")
+person=input("person: ")
+madLib=f"computer programming is so {ad}: it makes me so excited becaus i love to {verb} .stay hydrtaed and{ verb2} like you are {person}" 
+print(madLib)
